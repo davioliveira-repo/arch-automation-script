3b8cea0dd8d22b18915a373e56a14544be92458a
Davi Oliveira de Sá <eudavioliveira87@gmail.com>
2025-07-27 15:14:20 -0300

    traduzi para o inglês e adicionei comandos

--- setup_arch.sh
+++ setup_arch.sh
@@ -1,8 +1,17 @@
 # Verificar se o código está sendo executado como root
 if [[ $EUID -ne 0 ]]; then
-   echo "Este script deve ser executado como root."
+   echo "This script must be run as root"
    exit 1
 fi
 
-echo "Instalando o Nano, Git e Fastfetch..."
-pacman -S nano git fastfetch
+echo "Installing Nano, Git and Fastfetch..."
+pacman -S nano git fastfetch -y
+
+echo "Installing Firewall..."
+pacman -S gufw -y
+
+echo "Installing Codecs"
+pacman -S ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer -y
+
+echo "Installing Firmware Manager..."
+pacman -S fwupd
